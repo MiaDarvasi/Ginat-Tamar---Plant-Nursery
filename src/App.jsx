6de934c952f0b2eb039f9b8e95c1c60a1add2ee1
Mia Darvasi <miadarv@gmail.com>
2024-12-12 20:18:16 +0200

    add hebrew accesibility toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,120 @@
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { useEffect } from 'react';
+
+// import { Accessibility } from 'accessibility';
+
+// import { AppHeader } from './cmps/AppHeader.jsx';
+// import { Connect } from './cmps/Connect.jsx';
+
+// import { HomePage } from './pages/HomePage.jsx';
+// import { ScrollToTop } from './cmps/ScrollToTop.jsx';
+// import { About } from './pages/About.jsx';
+// import { Categories } from './pages/Categories.jsx';
+// import { Crystal } from './pages/Crystal.jsx';
+// import { Gallery } from './pages/Gallery.jsx';
+
+// import { Succulents } from './pages/categories/Succulents.jsx';
+// import { Herbs } from './pages/categories/Herbs.jsx';
+// import { Indoor } from './pages/categories/Indoor.jsx';
+// import { Pots } from './pages/categories/Pots.jsx';
+// import { Vining } from './pages/categories/Vining.jsx';
+// import { Trees } from './pages/categories/Trees.jsx';
+// import { Flowers } from './pages/categories/Flowers.jsx';
+
+// function App() {
+
+//   useEffect(() => {
+//     const labels = {
+//       resetTitle: 'איפוס',
+//       closeTitle: 'סגור',
+//       menuTitle: ' תפריט נגישות',
+//       increaseText: 'הגדל גודל גופן',
+//       decreaseText: 'הקטן גודל גופן',
+//       increaseTextSpacing: 'הגדל ריווח טקסט',
+//       decreaseTextSpacing: 'הקטן ריווח טקסט',
+//       increaseLineHeight: 'הגדל גובה שורה',
+//       decreaseLineHeight: 'הקטן גובה שורה',
+//       invertColors: 'הפוך צבעים',
+//       grayHues: 'גוונים אפורים',
+//       underlineLinks: 'קו תחתון לקישורים',
+//       bigCursor: 'סמן גדול',
+//       readingGuide: 'מדריך קריאה',
+//       textToSpeech: 'טקסט לדיבור',
+//       disableAnimations: 'השבתת אנימציות',
+//       hotkeyPrefix: 'קיצור דרך:'
+//     };
+
+//     const options = {
+//       labels: labels,
+//     };
+
+//     const accessibility = new Accessibility(options);
+
+//     const closeToolbar = (event) => {
+//       console.log("Close button clicked");
+//       const toolbar = document.querySelector('._access-menu');
+//       if (!toolbar.classList.contains('close')) {
+//           toolbar.classList.add('close');
+//       }
+//   };
+
+//   // Attempt to attach the close button event listener immediately
+//   const closeButton = document.querySelector('._menu-close-btn');
+//   if (closeButton) {
+//       console.log('Close button found');
+//       closeButton.addEventListener('click', closeToolbar);
+//   } else {
+//       console.log('Close button not found');
+//   }
+
+
+//     document.addEventListener('click', closeToolbar);
+
+//     document.body.setAttribute('dir', 'rtl');
+//     document.body.style.direction = 'rtl';
+//     return () => {
+//       // document.removeEventListener('click', closeToolbar);
+//       if (closeButton) {
+//         closeButton.removeEventListener('click', closeToolbar); // Clean up the event listener
+//     }
+//     };
+//   }, []);
+
+//   return (
+//     <Router>
+//       <ScrollToTop />
+//       <div className="App">
+//         <AppHeader />
+//         <Routes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/categories" element={<Categories />} />
+//           <Route path="/categories/succulents" element={<Succulents />} />
+//           <Route path="/categories/flowers" element={<Flowers />} />
+//           <Route path="/categories/herbs" element={<Herbs />} />
+//           <Route path="/categories/indoor" element={<Indoor />} />
+//           <Route path="/categories/pots" element={<Pots />} />
+//           <Route path="/categories/vining" element={<Vining />} />
+//           <Route path="/categories/trees" element={<Trees />} />
+//           <Route path="/crystal" element={<Crystal />} />
+//           <Route path="/gallery" element={<Gallery />} />
+//         </Routes>
+//         <Connect />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
 
 import { Accessibility } from 'accessibility';
+
+import { toggleMenu } from './main.js';
 
 import { AppHeader } from './cmps/AppHeader.jsx';
 import { Connect } from './cmps/Connect.jsx';
@@ -24,9 +137,29 @@ import { Flowers } from './pages/categories/Flowers.jsx';
 function App() {
 
   useEffect(() => {
-    window.addEventListener('load', function() {
-      new Accessibility();
-  }, false);
+    const labels = {
+      menuTitle: ' תפריט נגישות',
+      increaseText: 'הגדל גודל גופן',
+      decreaseText: 'הקטן גודל גופן',
+      increaseTextSpacing: 'הגדל ריווח טקסט',
+      decreaseTextSpacing: 'הקטן ריווח טקסט',
+      increaseLineHeight: 'הגדל גובה שורה',
+      decreaseLineHeight: 'הקטן גובה שורה',
+      invertColors: 'הפוך צבעים',
+      grayHues: 'גוונים אפורים',
+      underlineLinks: 'קו תחתון לקישורים',
+      bigCursor: 'סמן גדול',
+      readingGuide: 'מדריך קריאה',
+      textToSpeech: 'טקסט לדיבור',
+      disableAnimations: 'השבתת אנימציות',
+      hotkeyPrefix: 'קיצור דרך:'
+    };
+
+    const options = {
+      labels: labels,
+    };
+
+    const accessibility = new Accessibility(options);
   }, []);
 
   return (
@@ -34,6 +167,7 @@ function App() {
       <ScrollToTop />
       <div className="App">
         <AppHeader />
+        {/* <div onClick={() => toggleMenu()}>CLOSE MENU HOPEFULLY</div> */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<About />} />
--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,3 +1,64 @@
+// import { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+
+// import trees from "../assets/imgs/trees.svg";
+// import phone from "../assets/imgs/icons/phone.svg";
+
+
+// export function AppHeader() {
+//   const [isScrolled, setIsScrolled] = useState(false);
+//   const [isActive, setIsActive] = useState(null);
+
+//   const handleScroll = () => {
+//     if (window.scrollY > 50) {
+//       setIsScrolled(true);
+//     } else {
+//       setIsScrolled(false);
+//     }
+//   }; 
+
+//   useEffect(() => {
+//     window.addEventListener('scroll', handleScroll);
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+
+//   return (
+//     <section className={`app-header full ${isScrolled ? 'scrolled' : ''}`}>
+//       <section>
+//         <section className="app-header-content">
+//           <section className="header-contact">
+//             <Link to="/">
+//               <section className="logo">
+//                 <img src={trees} alt="logo" />
+//                 <p>גינת תמר</p>
+//               </section>
+//             </Link>
+//             <h1>גינת תמר</h1>
+//             <section className="header-call">
+//               <a href="tel:02-6710335"><img src={phone} /></a>
+//               <a href="tel:02-6710335"><p>02-6710335</p></a>
+//             </section>
+//           </section>
+//         </section>
+//         <hr />
+//         <nav>
+//           <a href="#connect">דרכי התקשרות</a>
+//           <Link to="/gallery" onClick={setIsActive("gallery")} className={isActive==="gallery"? "active" : ""}>גלריה</Link>
+//           <Link to="/crystal" onClick={setIsActive("crystal")}>קריסטלים</Link>
+//           <Link to="/categories" onClick={setIsActive("categories")}>צמחים</Link>
+//           <Link to="/about" onClick={setIsActive("about")} >קצת עלינו</Link>
+//           <Link to="/" >בית</Link>
+//         </nav>
+//       </section>
+//     </section>
+//   );
+// }
+
+
+
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -7,6 +68,7 @@ import phone from "../assets/imgs/icons/phone.svg";
 
 export function AppHeader() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isActive, setIsActive] = useState(null);
 
   const handleScroll = () => {
     if (window.scrollY > 50) {
@@ -22,6 +84,10 @@ export function AppHeader() {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const handleLinkClick = (link) => {
+    setIsActive(link);
+  };
 
   return (
     <section className={`app-header full ${isScrolled ? 'scrolled' : ''}`}>
@@ -43,12 +109,12 @@ export function AppHeader() {
         </section>
         <hr />
         <nav>
-          <a href="#connect">דרכי התקשרות</a>
-          <Link to="/gallery">גלריה</Link>
-          <Link to="/crystal">קריסטלים</Link>
-          <Link to="/categories">צמחים</Link>
-          <Link to="/about" >קצת עלינו</Link>
-          <Link to="/" >בית</Link>
+          <a href="#connect" onClick={() => handleLinkClick(null)}>דרכי התקשרות</a>
+          <Link to="/gallery" onClick={() => handleLinkClick("gallery")} className={isActive === "gallery" ? "active" : ""}>גלריה</Link>
+          <Link to="/crystal" onClick={() => handleLinkClick("crystal")} className={isActive === "crystal" ? "active" : ""}>קריסטלים</Link>
+          <Link to="/categories" onClick={() => handleLinkClick("categories")} className={isActive === "categories" ? "active" : ""}>צמחים</Link>
+          <Link to="/about" onClick={() => handleLinkClick("about")} className={isActive === "about" ? "active" : ""}>קצת עלינו</Link>
+          <Link to="/" onClick={() => handleLinkClick(null)}>בית</Link>
         </nav>
       </section>
     </section>
